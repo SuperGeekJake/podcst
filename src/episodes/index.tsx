b7ae78ca1e83a539f09aa372c9c7b1ffb90385ad
Shantanu Raj <s@sraj.me>
2022-03-20 17:26:18 +0100

    feat(episodes): Add stub podcast actions

--- src/episodes/index.tsx
+++ src/episodes/index.tsx
@@ -3,6 +3,7 @@ import { useSearchParams } from "solid-app-router";
 
 import { getPodcast } from "@src/api";
 import { getTextContent } from "@src/utils";
+import { Button } from "@src/ui/Button";
 
 export const Episodes: Component = () => {
   const [searchParams] = useSearchParams();
@@ -17,10 +18,18 @@ export const Episodes: Component = () => {
           alt={data()?.cover}
           title={data()?.cover}
         />
-        <div class="flex-1">
+        <div class="flex flex-1 flex-col gap-4">
           <h1 class="text-6xl font-display font-bold">{data()?.title}</h1>
           <h2 class="text-4xl font-display">{data()?.author}</h2>
           <div class="max-w-4xl">{description()}</div>
+          <div class="flex gap-4">
+            <Button>
+              Subscribe
+            </Button>
+            <Button>
+              Share
+            </Button>
+          </div>
         </div>
       </div>
     </main>
