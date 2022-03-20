f46682555fe58ba4a7bfab4f8434c2fc9d70bb7c
Shantanu Raj <s@sraj.me>
2022-03-20 23:47:24 +0100

    feat(home): Hide subs if no subs

--- src/home/CategorySubs.tsx
+++ src/home/CategorySubs.tsx
@@ -8,6 +8,9 @@ import { useSubscriptionsContext } from "@src/subscriptions";
 export const CategorySubs: Component = () => {
   const store = useSubscriptionsContext();
   const { subscriptionList } = store;
+  if (subscriptionList().length === 0) {
+    return null;
+  }
   return (
     <PodcastsGrid title="Subscriptions" podcasts={subscriptionList()} />
   );
