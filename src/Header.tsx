aa15e940426e9d636ceb5f7656dceea473fe4a45
Jacob Williams <supergeekjake@gmail.com>
2022-03-22 20:26:09 -0700

    fix(ui): Correct header background styles

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,7 +7,7 @@ export const Header: Component = () => {
   const [searchVisiblity, setSearchVisiblity] = createSignal<boolean>(false);
   return (
     <header
-      class="fixed top-0 left-0 z-10 flex items-center justify-start w-screen h-16 px-12 bg-bla hover:bg-black/95 transition-all"
+      class="fixed top-0 left-0 z-20 flex items-center justify-start w-screen h-16 px-12 bg-black/0 hover:bg-black/95 transition-all"
       data-component="Header"
     >
       <div class="absolute top-0 bottom-0 left-1/2 right-auto -translate-x-1/2 flex justify-center items-center">
